b899c188a10770d91cd5ba1d28ef8e44fba15c9d
Petter Uvesten <uvesten@everichon.com>
2022-09-12 20:31:12 +0200

    Add naiveSync file read command

---
+++ src/naiveSync.ts
@@ -0,0 +1,29 @@
+import { readFileSync, writeFileSync } from "fs";
+
+// read command line args
+const argv = process.argv;
+
+const inFilePath = argv[2]; // not much error checking going on here
+const outFilePath = argv[3];
+
+if (!(inFilePath && outFilePath)) {
+  console.error(
+    `This command needs to be called with inFilePath and outFilePath`
+  );
+  process.exit(1);
+}
+
+const getSum = /^([^;]+;){5} withdrawal; (?<withdrawalSum>[0-9.]+); USD$/;
+
+const inFile = readFileSync(inFilePath, { encoding: "utf-8" });
+const output: string[] = [];
+
+for (const line of inFile.split("\n")) {
+  const res = getSum.exec(line);
+  const sum = parseFloat(res?.groups?.["withdrawalSum"] ?? "0");
+  if (sum > 1_000_000) {
+    output.push(line);
+  }
+}
+
+writeFileSync(outFilePath, output.join("\n"));
